2a18df0b9f43346f7bc49f6b4489d2dfb584427a
chanhong-ji <jichanhong24@gmail.com>
2022-03-14 19:22:00 +0900

    feat: Add Login Mutation

--- users/user.mutations.ts
+++ users/user.mutations.ts
@@ -1,11 +1,17 @@
 import client from "../client";
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
+import jwt from "jsonwebtoken";
 
 interface ICreate {
   firstName: string;
   lastName: string;
   username: string;
   email: string;
+  password: string;
+}
+
+interface ILogin {
+  username: string;
   password: string;
 }
 
@@ -35,6 +41,33 @@ export default {
         console.log("Create account error", error);
         return error;
       }
+    },
+
+    login: async (_: any, { username, password }: ILogin) => {
+      // find user with username
+      const user = await client.user.findFirst({ where: { username } });
+      if (!user) {
+        return {
+          ok: false,
+          error: "User not found",
+        };
+      }
+      // check password with db password
+      const passwordOk = await compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Password Wrong",
+        };
+      }
+      // issue token
+      const token = jwt.sign({ id: user.id }, process.env.PRIVATE_KEY ?? "", {
+        expiresIn: "14d",
+      });
+      return {
+        ok: true,
+        token,
+      };
     },
   },
 };
--- users/user.typeDefs.ts
+++ users/user.typeDefs.ts
@@ -11,6 +11,12 @@ export default gql`
     updatedAt: String
   }
 
+  type LoginResult {
+    ok: Boolean!
+    token: String
+    error: String
+  }
+
   type Query {
     seeProfile(username: String!): User
   }
@@ -23,5 +29,7 @@ export default gql`
       email: String!
       password: String!
     ): User
+
+    login(username: String!, password: String!): LoginResult
   }
 `;
