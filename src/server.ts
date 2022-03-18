7e5c82d76aa0eb3dcf748d1177409b238f552c1b
chanhong-ji <jichanhong24@gmail.com>
2022-03-18 19:47:16 +0900

    feat: Upload file
    with Apollo server express

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,34 @@
 import "dotenv/config";
-import { ApolloServer } from "apollo-server";
+const { ApolloServer } = require("apollo-server-express");
 import schema from "./schema";
 import { getUser } from "./users/user.utils";
-
-const server = new ApolloServer({
-  schema,
-  // get token from http headers and send user data to all resolvers
-  context: async ({ req }) => {
-    const { token } = req.headers;
-    if (!token || typeof token !== "string") return null;
-    return { loggedInUser: await getUser(token) };
-  },
-});
+import express from "express";
+import { graphqlUploadExpress } from "graphql-upload";
+import morgan from "morgan";
 
 const PORT = process.env.PORT;
-server
-  .listen(PORT)
-  .then(() => console.log(`✨Server is running on http://localhost:${PORT}/`));
+
+async function startServer() {
+  const apolloServer = new ApolloServer({
+    schema,
+    context: async ({ req }: any) => {
+      const { token } = req.headers;
+      if (!token || typeof token !== "string") return null;
+      return { loggedInUser: await getUser(token) };
+    },
+  });
+  await apolloServer.start();
+
+  const app = express();
+
+  app.use(graphqlUploadExpress());
+  app.use(morgan("dev"));
+  app.use("/static", express.static("uploads"));
+  apolloServer.applyMiddleware({ app });
+
+  await new Promise<void>((r) => app.listen({ port: PORT }, r));
+
+  console.log(`🚀 Server ready at http://localhost:${PORT}`);
+}
+
+startServer();
--- src/users/editProfile/editProfile.resolvers.ts
+++ src/users/editProfile/editProfile.resolvers.ts
@@ -2,18 +2,40 @@ import { hash } from "bcrypt";
 import { Resolvers } from "../../types";
 import { protectedResolver } from "../user.utils";
 import client from "../../client";
+import { createWriteStream } from "fs";
 
 const resolvers: Resolvers = {
   Mutation: {
     editProfile: protectedResolver(
       async (
         _: any,
-        { firstName, lastName, username, email, password: newPassword },
+        {
+          firstName,
+          lastName,
+          username,
+          email,
+          password: newPassword,
+          bio,
+          avatar,
+        },
         { loggedInUser }
       ) => {
         // check user authenthcation
         if (!loggedInUser) {
           return { ok: false };
+        }
+
+        // if avatar, add image file
+        let avatarUrl = null;
+        if (avatar) {
+          const { filename, createReadStream } = await avatar;
+          const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
+          const readStream = createReadStream();
+          const writeStream = createWriteStream(
+            process.cwd() + "/uploads/" + newFilename
+          );
+          readStream.pipe(writeStream);
+          avatarUrl = `http://localhost:6001/static/${newFilename}`;
         }
 
         // check if there is password input
@@ -31,9 +53,13 @@ const resolvers: Resolvers = {
             lastName,
             username,
             email,
+            bio,
             ...(updatedPassword && { password: updatedPassword }),
+            ...(avatarUrl && { avatar: avatarUrl }),
           },
         });
+        console.log(updatedUser);
+
         if (updatedUser.id) {
           return { ok: true };
         } else {
