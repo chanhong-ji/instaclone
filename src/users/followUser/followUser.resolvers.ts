2dd8e79dc40fe48c64740f1836d3e1adbd9e4587
chanhong-ji <jichanhong24@gmail.com>
2022-03-21 19:59:58 +0900

    fix: Prevent to follow self (followUser)

--- src/users/followUser/followUser.resolvers.ts
+++ src/users/followUser/followUser.resolvers.ts
@@ -4,38 +4,43 @@ import { protectedResolver } from "../user.utils";
 
 const resolvers: Resolvers = {
   Mutation: {
-    followUser: protectedResolver(async (_, { username }, { loggedInUser }) => {
-      // check if user with username
-      const ok = await client.user.findFirst({ where: { username } });
-      if (!ok) {
-        return {
-          ok: false,
-          error: "username does not exist",
-        };
-      }
-      //   update follower
-      try {
-        await client.user.update({
-          where: { id: loggedInUser.id },
-          data: {
-            following: {
-              connect: {
-                username,
+    followUser: protectedResolver(
+      async (_, { username }, { loggedInUser: { id: userId } }) => {
+        // check if user with username
+        const user = await client.user.findFirst({ where: { username } });
+        if (!user) {
+          return {
+            ok: false,
+            error: "username does not exist",
+          };
+        }
+        // unable to follow self
+        if (user.id === userId)
+          return { ok: false, error: "Not allow to follow yourself" };
+        //   update follower
+        try {
+          await client.user.update({
+            where: { id: userId },
+            data: {
+              following: {
+                connect: {
+                  username,
+                },
               },
             },
-          },
-        });
-        return {
-          ok: true,
-        };
-      } catch (error) {
-        console.log("update follower error");
-        return {
-          ok: false,
-          error,
-        };
+          });
+          return {
+            ok: true,
+          };
+        } catch (error) {
+          console.log("update follower error");
+          return {
+            ok: false,
+            error,
+          };
+        }
       }
-    }),
+    ),
   },
 };
 
