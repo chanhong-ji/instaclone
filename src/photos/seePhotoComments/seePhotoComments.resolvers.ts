5d3b8527646af2c363d621ba131252bafd3c3550
chanhong-ji <jichanhong24@gmail.com>
2022-03-21 23:08:39 +0900

    feat: Add seePhotoComments Query

---
+++ src/photos/seePhotoComments/seePhotoComments.resolvers.ts
@@ -0,0 +1,20 @@
+import client from "../../client";
+import { Resolvers } from "../../types";
+
+const COMMENT_PAGE = 10;
+
+const resolvers: Resolvers = {
+  Query: {
+    seePhotoComments: async (_, { photoId, lastId }) => {
+      const comments = await client.comment.findMany({
+        where: { photoId },
+        take: COMMENT_PAGE,
+        skip: lastId ? 1 : 0,
+        ...(lastId && { cursor: { id: lastId } }),
+      });
+      return comments;
+    },
+  },
+};
+
+export default resolvers;
