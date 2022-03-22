18ddeccfe4d02358e1818ca816144fe0b3c847a5
chanhong-ji <jichanhong24@gmail.com>
2022-03-22 17:21:32 +0900

    refactor: Create shared typeDefs

--- src/photos/uploadPhoto/uploadPhoto.typeDefs.ts
+++ src/photos/uploadPhoto/uploadPhoto.typeDefs.ts
@@ -1,11 +1,7 @@
 import { gql } from "apollo-server";
 
 export default gql`
-  type UploadPhotoResult {
-    ok: Boolean!
-    error: String
-  }
   type Mutation {
-    uploadPhoto(file: Upload!, caption: String): UploadPhotoResult
+    uploadPhoto(file: Upload!, caption: String): MutationResponse!
   }
 `;
