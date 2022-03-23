4b838e5db48a2989a79111573be38cc79d2ac75e
chanhong-ji <jichanhong24@gmail.com>
2022-03-23 22:56:59 +0900

    feat: Set up Subscriptions

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
 import "dotenv/config";
-const { ApolloServer } = require("apollo-server-express");
-import schema from "./schema";
-import { getUser } from "./users/user.utils";
+import { ApolloServer } from "apollo-server-express";
 import express from "express";
-import { graphqlUploadExpress } from "graphql-upload";
+import { createServer } from "http";
+import schema from "./schema";
 import morgan from "morgan";
+import { getUser } from "./users/user.utils";
+import { graphqlUploadExpress } from "graphql-upload";
+import { execute, subscribe } from "graphql";
+import { SubscriptionServer } from "subscriptions-transport-ws";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 
 const PORT = process.env.PORT;
 
 async function startServer() {
+  const app = express();
+  const httpServer = createServer(app);
   const apolloServer = new ApolloServer({
     schema,
     context: async ({ req }: any) => {
@@ -16,19 +22,43 @@ async function startServer() {
       if (!token || typeof token !== "string") return null;
       return { loggedInUser: await getUser(token) };
     },
+    plugins: [
+      ApolloServerPluginLandingPageGraphQLPlayground(),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              subscriptionServer.close();
+            },
+          };
+        },
+      },
+    ],
   });
+  const subscriptionServer = SubscriptionServer.create(
+    {
+      schema,
+      execute,
+      subscribe,
+      onConnect: () => {
+        // lookup userId by token, etc.
+      },
+    },
+    {
+      server: httpServer,
+      path: apolloServer.graphqlPath,
+    }
+  );
+
   await apolloServer.start();
-
-  const app = express();
-
   app.use(graphqlUploadExpress());
   app.use(morgan("dev"));
   app.use("/static", express.static("uploads"));
   apolloServer.applyMiddleware({ app });
 
-  await new Promise<void>((r) => app.listen({ port: PORT }, r));
-
-  console.log(`🚀 Server ready at http://localhost:${PORT}`);
+  httpServer.listen(PORT, () =>
+    console.log(`Server is now running on http://localhost:${PORT}/graphql`)
+  );
 }
 
 startServer();
